ca4be8dfb1eb5bb478c7acb2dad392d5855c7d8a
A-wolf <sadow.dracula@gmail.com>
2019-02-22 13:08:59 +0100

    c1

--- src/Grupp/Volvo740.java
+++ src/Grupp/Volvo740.java
@@ -1,19 +1,28 @@
 package Grupp;
 import javafx.scene.paint.Color;
 
+	/**
+	 * @author Bosses00 and MercuryBarium
+	 *
+	 */
 	public class Volvo740 extends Car {
-	private final static double trimFactor = 1.25;
+	private final static double trimFactor = 1.25;	
 
-	//Description of the Volvo740	
-
+	/**
+	 * 
+	 * A description of the aspects of an Volvo740
+	 * 
+	 */
 	public Volvo740() {
 		color = Color.RED;
 		enginePower = 100;
 		modelName = "Volvo740";
 		stopEngine();
 	}
-	// Unique speed modifier for Volvo740 
-	
+
+	/* 
+	 * The unique speed modifier of the volvo740
+	 */
 	public double speedFactor() {
 		return enginePower * 0.01 * trimFactor;
 	}
