b1312e7d730aad8eab36aec3a81a11cae08605e6
bosses00 <hampus-andre@hotmail.se>
2019-02-25 14:41:59 +0100

    updatescan

--- src/Grupp/Scania.java
+++ src/Grupp/Scania.java
@@ -3,8 +3,8 @@ import javafx.scene.paint.Color;
 	
 	
 	public class Scania extends Car {
+		private int flakDegresOpen = 0;
 		private boolean flakOpen = false;
-
 	//Description of the Scania	
 
 	public Scania() {
@@ -18,16 +18,29 @@ import javafx.scene.paint.Color;
 	public double speedFactor() {
 		return enginePower * 0.01;
 	}
-
+	
 	public void flakOpen(){
-		flakOpen = true;
+		if (currentSpeed > 0){
+			return;
+		}else{
+		for(flakDegresOpen = 0; flakDegresOpen <= 70; flakDegresOpen++){
+			if(flakDegresOpen == 70){
+				flakOpen = true;
+			}
+			}
+		
+		}
+		
 	}
 	public void flakClose(){
-		flakOpen = false;
+		for(flakDegresOpen = 0; flakDegresOpen >= 0; flakDegresOpen--){
+			if(flakDegresOpen == 0){
+				flakOpen = false;
+			}
 	}
 
 
 
 
-
+	}
 }
