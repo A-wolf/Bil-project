399d39c9e22ed0ff4dc197b0cb62262462319d52
bosses00 <hampus-andre@hotmail.se>
2019-05-03 13:04:34 +0200

    Latillsaaboscania

--- src/view/CarPane.java
+++ src/view/CarPane.java
@@ -7,22 +7,31 @@ import javafx.scene.layout.Pane;
 public class CarPane extends Pane {
 
 	private ImageView volvoImage;
+	private ImageView scaniaImage;
+	private ImageView saab95Image;
 	private double xPos;
 	private double yPos;
 
 	public CarPane() {
 		volvoImage = new ImageView(new Image("/pics/Volvo740.jpg"));
-		this.getChildren().add(volvoImage);
+		scaniaImage = new ImageView(new Image("/pics/Scania.jpg"));
+		saab95Image = new ImageView(new Image("/pics/Saab95.jpg"));
+		this.getChildren().addAll(volvoImage, scaniaImage, saab95Image);
 	}
 
 	public void move(double x, double y) {
 		xPos = x;
 		yPos = y;
-	}
+		
+		}
 
 	public void update() {
 		volvoImage.setX(xPos);
 		volvoImage.setTranslateY(yPos);
+		scaniaImage.setX(xPos);
+		scaniaImage.setTranslateY(yPos+100);
+		saab95Image.setX(xPos);
+		saab95Image.setTranslateY(yPos+200);
 	}
 
 }
