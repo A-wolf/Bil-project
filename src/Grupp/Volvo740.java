ecd412aa86f5d3a4918abca16e960f4319b16189
MercuryBarium <marcus.brulls@gmail.com>
2019-05-13 14:50:00 +0200

    BlablaBla

--- src/Grupp/Volvo740.java
+++ src/Grupp/Volvo740.java
@@ -1,17 +1,17 @@
 package Grupp;
+
 import javafx.scene.paint.Color;
 
-	/**
-	 * @author Bosses00 and MercuryBarium
-	 *
-	 */
-	public class Volvo740 extends Vehicle {
+/**
+ * @author Bosses00 and MercuryBarium
+ *
+ */
+public class Volvo740 extends Vehicle {
 	private final static double trimFactor = 1.25;
 	public double weight = 1;
 
 	/**
-	 * @see Grupp.Volvo740#Volvo740
-	 * A description of the aspects of a Volvo740
+	 * @see Grupp.Volvo740#Volvo740 A description of the aspects of a Volvo740
 	 * 
 	 */
 	public Volvo740() {
@@ -21,18 +21,11 @@ import javafx.scene.paint.Color;
 		stopEngine();
 	}
 
-	
 	/*
 	 * @see Grupp.Car#speedFactor()
 	 */
 	public double speedFactor() {
 		return enginePower * 0.01 * trimFactor;
 	}
-
-	
-
-
-
-
 
 }
